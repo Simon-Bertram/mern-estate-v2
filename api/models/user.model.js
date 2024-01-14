7758ec2195b29278d6d6f6af23e8d054384890ac
Simon Bertram <simonbertram@hotmail.com>
2024-01-14 16:26:41 +0000

    Create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 40,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 255,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
