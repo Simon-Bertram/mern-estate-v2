177cf52ae7a2583fa0c6839ed4843fdb708b3b62
Simon Bertram <simonbertram@hotmail.com>
2024-01-15 23:23:41 +0000

    Create sign in form UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,43 @@
+import { Link } from "react-router-dom"
+
 const SignUp = () => {
-  return <div>Sign Out</div>
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          id="username"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          id="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+        />
+        <button className="btn bg-green-600 text-white p-3">Sign Up</button>
+        <button className="btn bg-blue-500 text-white p-3">
+          Continue with Google
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-600 font-semibold hover:underline underline-offset-4">
+            Sign In
+          </span>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default SignUp
