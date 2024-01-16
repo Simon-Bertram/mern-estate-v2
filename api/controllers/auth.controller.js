56697e73e96e3969b9203cd41bc09d273b143a31
Simon Bertram <simonbertram@hotmail.com>
2024-01-16 11:47:21 +0000

    Create sign-in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
@@ -9,6 +10,33 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(200).json("User created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "Invalid email or password"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid email or password"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {});
+    // Remove the password from the response
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        maxAge: 86400,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
